fc8390d88d9c54101d556ae650dc9d5eef2b7a87
Mabe-Diaby <diabymabe@gmail.com>
2022-01-06 16:36:57 -0500

    part 2

--- challenge.py
+++ challenge.py
@@ -1,30 +1,65 @@
 ######################################################
-# Part 1: Most Clocks are Normal, But Some are Cuckoo
-time = int(input("What time is it using Miltary time?(0,23)"))
+# # Part 1: Most Clocks are Normal, But Some are Cuckoo
+# time = int(input("What time is it using Miltary time?(0,23)"))
 
-# Complete the prompt here
+# # Complete the prompt here
 
-if time < 9:
-    print("Morning is wonderful. Its only drawback is that it comes at such an inconvenient time of day.")
-elif time <=16:
-    print("Working hard or hardly working?")
-elif time < 20:
-    print("How did it get so late so soon?")
-else:
-    print("Burning the midnight oil!")
+# if time < 9:
+#     print("Morning is wonderful. Its only drawback is that it comes at such an inconvenient time of day.")
+# elif time <=16:
+#     print("Working hard or hardly working?")
+# elif time < 20:
+#     print("How did it get so late so soon?")
+# else:
+#     print("Burning the midnight oil!")
 
 ######################################################
 # Part 2: I Came, I 'Saur, I Conquered
 
-angry = True
-bored = True
-hungry = False
-tired = False
+angry = False
+bored = False
+hungry = True
+tired = True
+
+# t_rex = input("How is T-Rex?")
 
 # Complete the prompt here
 # Example `if` statement:
 if bored:
     print('T-Rex roars! RAWR!')
+if angry and hungry and bored:
+    print('T-Rex stummy growl! He eats the Triceratops')
+elif tired and hungry:
+    print('T-Rex stummy growl! He eats the Iguanadon and Yawns')
+elif hungry and bored:
+    print('T-Rex stummy growl! He eats the Stegasaurus T-Rex roars! RAWR!')
+elif tired:
+    print("T-rex yawns! He goes to sleep")
+elif angry and bored:
+    print("What ever will T-rex do? He fights with the Velociraptor")
+elif angry or bored:
+    print('T-Rex roars! RAWR!')
+else:
+    print('T-Rex gives a toothy smile')
+    
+# or
+
+if angry and hungry and bored:
+    print('T-Rex stummy growl! He eats the Triceratops')
+elif tired and hungry:
+    print('T-Rex stummy growl! He eats the Iguanadon and Yawns')
+elif hungry and bored:
+    print('T-Rex stummy growl! He eats the Stegasaurus T-Rex roars! RAWR!')
+elif tired:
+    print("T-rex yawns! He goes to sleep")
+elif angry and bored:
+    print("What ever will T-rex do? He fights with the Velociraptor")
+elif angry or bored:
+    print('T-Rex roars! RAWR!')
+elif bored:
+    print('T-Rex roars! RAWR!')
+else:
+    print('T-Rex gives a toothy smile')
 
 ######################################################
 # Part 3: IOU
